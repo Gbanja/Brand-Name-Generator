b4d75cccaee01afbb5c70679fa1971e416dcaf54
Gbanjah Campbell <134650831+Gbanja@users.noreply.github.com>
2026-02-14 15:53:43 +0000

    Add brand name generator functionality

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to my awesome brand name generator program")
+user_city = input("What is the name of the city you grew up in?")
+user_pet_name = input("What is the name of the pet?")
+print(f"Your brand name could be {user_city} {user_pet_name}")
